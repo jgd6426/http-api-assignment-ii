32296cf3b1b96d82058c11967496fb06cbdc9a8c
Judy Derrick <jgd6426@rit.edu>
2023-10-01 23:32:31 -0400

    mostly finished http II assignment

---
+++ src/jsonResponses.js
@@ -0,0 +1,97 @@
+const users = {};
+
+// function to respond with a json object
+const respondJSON = (request, response, status, object) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  // send response with json object
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+// function to respond without json body
+const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  // send response without json object, just headers
+  response.writeHead(status, headers);
+  response.end();
+};
+
+// get user object
+const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
+  // return 200 with message
+  return respondJSON(request, response, 200, responseJSON.users);
+};
+
+// get meta info about user object
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+// function to add a user from a POST body
+const addUser = (request, response, body) => {
+  // default json message
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  // check to make sure we have both fields
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  // default status code to 204 updated
+  let responseCode = 204;
+
+  // If the user doesn't exist yet
+  if (!users[body.name]) {
+    // set the status code to 201 (created) and create an empty user
+    responseCode = 201;
+    users[body.name] = {};
+  }
+
+  // add or update fields for this user name
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  // if response is created, then set our created message
+  // and send response with a message
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+// function for 404 not found requests with message
+const notFound = (request, response) => {
+  // create error message for response
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return a 404 with an error message
+  respondJSON(request, response, 404, responseJSON);
+};
+
+// function for 404 not found without message
+const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
+
+// set public modules
+module.exports = {
+  addUser,
+  getUsers,
+  getUsersMeta,
+  notFound,
+  notFoundMeta,
+};
---
+++ src/server.js
@@ -0,0 +1,82 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// Recompiles the body of a request, and then calls the appropriate handler once completed.
+const parseBody = (request, response, handler) => {
+  // The request will come in in pieces. We will store those pieces in this
+  // body array.
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    handler(request, response, bodyParams);
+  });
+};
+
+// handle POST requests
+const handlePost = (request, response, parsedUrl) => {
+  // If they go to /addUser
+  if (parsedUrl.pathname === '/addUser') {
+    parseBody(request, response, jsonHandler.addUser);
+  }
+};
+
+// handle GET requests
+const handleGet = (request, response, parsedUrl) => {
+  // route to correct method based on url
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else if (parsedUrl.pathname === '/notReal') {
+    jsonHandler.notFound(request, response);
+  } else {
+    htmlHandler.getIndex(request, response);
+  }
+};
+
+// handle HEAD requests
+const handleHead = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsersMeta(request, response);
+  } else if (parsedUrl.pathname === '/notReal') {
+    jsonHandler.notFoundMeta(request, response);
+  } else {
+    htmlHandler.notFoundMeta(request, response);
+  }
+};
+
+const onRequest = (request, response) => {
+  // parse information from the url
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else if (request.method === 'GET') {
+    handleGet(request, response, parsedUrl);
+  } else if (request.method === 'HEAD') {
+    handleHead(request, response, parsedUrl);
+  }
+};
+
+// start server
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
